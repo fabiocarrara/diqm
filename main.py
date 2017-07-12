f008395f1f54df10a618eff013ad475d51c7cf09
Fabio Carrara <fabio.carrara@isti.cnr.it>
2017-07-12 14:44:32 +0200

    first working code

---
+++ main.py
@@ -0,0 +1,63 @@
+import argparse
+from model import UNet
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import tensorflow as tf
+import numpy as np
+
+
+def load_pipeline(fnames):
+    queue = tf.train.string_input_producer(fnames)
+    reader = tf.WholeFileReader()
+    _, content = reader.read(queue)
+    image = tf.image.decode_png(content)
+    float_image = tf.cast(image, dtype=tf.float32)
+    return float_image
+
+
+def load_image_to_tensor(fname, grayscale=False):
+    img = load_img(fname, grayscale=grayscale)
+    x = img_to_array(img) / 255.0
+    x = x.reshape((1,) + x.shape)
+    return x
+
+
+def main(args):
+
+    model = UNet().create_model(img_shape=(512, 512, 3), num_class=1)
+    model.compile(loss=['binary_crossentropy', 'mse'],
+                 optimizer='adam',)
+                 # metrics=['binary_crossentropy', 'mse'])
+
+    reference_inputs = ['reference.png']
+    distorted_inputs = ['distorted.png']
+    pmaps_inputs = ['P_map.png']
+    q_inputs = np.array([[38.56 / 100]])
+
+    # reference = load_pipeline(reference_inputs)
+    # distorted = load_pipeline(distorted_inputs)
+    # pmap = load_pipeline(pmaps_inputs)
+
+    reference = load_image_to_tensor(reference_inputs[0])
+    distorted = load_image_to_tensor(distorted_inputs[0])
+    pmap = load_image_to_tensor(pmaps_inputs[0], grayscale=True)
+    q = q_inputs
+
+    # print reference
+    # print distorted
+    # print pmap
+
+    model.fit([reference, distorted], [pmap, q], batch_size=1, epochs=50)
+    y, qpred = model.predict([reference, distorted])
+
+    print 'Q=', qpred
+    img = array_to_img(y[0]*255)
+    img.save('predicted_map.png')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PMap and Q predictor ?')
+    # parser.add_argument('data', default='data/', help='Directory containing image data')
+    args = parser.parse_args()
+    main(args)
+
+
---
+++ model.py
@@ -0,0 +1,100 @@
+from keras.models import Model
+from keras.layers import Input, MaxPooling2D, UpSampling2D, Cropping2D, ZeroPadding2D, merge, Dense, Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.merge import concatenate, Multiply
+
+import keras.backend as K
+
+class UNet():
+    def __init__(self):
+        print ('build UNet ...')
+
+    def get_crop_shape(self, target, refer):
+        # width, the 3rd dimension
+        cw = (target.get_shape()[2] - refer.get_shape()[2]).value
+        assert (cw >= 0)
+        if cw % 2 != 0:
+            cw1, cw2 = int(cw / 2), int(cw / 2) + 1
+        else:
+            cw1, cw2 = int(cw / 2), int(cw / 2)
+        # height, the 2nd dimension
+        ch = (target.get_shape()[1] - refer.get_shape()[1]).value
+        assert (ch >= 0)
+        if ch % 2 != 0:
+            ch1, ch2 = int(ch / 2), int(ch / 2) + 1
+        else:
+            ch1, ch2 = int(ch / 2), int(ch / 2)
+
+        return (ch1, ch2), (cw1, cw2)
+
+    def create_model(self, img_shape, num_class):
+
+        concat_axis = 3
+        references = Input(shape=img_shape)
+        distorted = Input(shape=img_shape)
+        inputs = merge([references, distorted], mode='concat')
+
+        conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', name='conv1_1')(inputs)
+        conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+        pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+        conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+        conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+        pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+        conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+        conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+        pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+        conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+        conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+        pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+        conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
+        conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+
+        up_conv5 = UpSampling2D(size=(2, 2))(conv5)
+        ch, cw = self.get_crop_shape(conv4, up_conv5)
+        crop_conv4 = Cropping2D(cropping=(ch, cw))(conv4)
+        up6 = concatenate([up_conv5, crop_conv4], axis=concat_axis)
+        conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
+        conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+
+        up_conv6 = UpSampling2D(size=(2, 2))(conv6)
+        ch, cw = self.get_crop_shape(conv3, up_conv6)
+        crop_conv3 = Cropping2D(cropping=(ch, cw))(conv3)
+        up7 = concatenate([up_conv6, crop_conv3], axis=concat_axis)
+        conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
+        conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+
+        up_conv7 = UpSampling2D(size=(2, 2))(conv7)
+        ch, cw = self.get_crop_shape(conv2, up_conv7)
+        crop_conv2 = Cropping2D(cropping=(ch, cw))(conv2)
+        up8 = concatenate([up_conv7, crop_conv2], axis=concat_axis)
+        conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
+        conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+
+        up_conv8 = UpSampling2D(size=(2, 2))(conv8)
+        ch, cw = self.get_crop_shape(conv1, up_conv8)
+        crop_conv1 = Cropping2D(cropping=(ch, cw))(conv1)
+        up9 = concatenate([up_conv8, crop_conv1], axis=concat_axis)
+        conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
+        conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+
+        ch, cw = self.get_crop_shape(inputs, conv9)
+        conv9 = ZeroPadding2D(padding=((ch[0], ch[1]), (cw[0], cw[1])))(conv9)
+        conv10 = Conv2D(num_class, (1, 1), activation='sigmoid')(conv9)
+
+        flat_conv5 = Flatten()(conv5)
+        fc1 = Dense(256, activation='relu')(flat_conv5)
+        q = Dense(1, activation='sigmoid')(fc1)
+
+        model = Model(inputs=[references, distorted], outputs=[conv10, q])
+
+        return model
+
+
+if __name__ == '__main__':
+    unet = UNet()
+    model = unet.create_model(img_shape=(512, 512, 3), num_class=1)
+
+    print model.summary()
